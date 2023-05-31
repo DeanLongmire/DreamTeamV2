f6249d437e073ff4adb287d59fe8df24211e193b
Dean Longmire <deanlongmire100@gmail.com>
2023-05-31 00:37:02 -0400

    Send email and password in post request

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,14 +1,23 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class LoginController extends Controller {
     @service router;
+    @tracked credentials = {
+        email: "",
+        password: ""
+    }
 
     @action
     loginClick() {
         fetch("http://localhost:5000/users/login", {
-            method: 'GET',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.credentials)
         })
         .then(response => {
             if(response.ok)
@@ -17,5 +26,15 @@ export default class LoginController extends Controller {
                 this.router.transitionTo('team-home');
             }   
         });
+    }
+
+    @action
+    updateEmail(event) {
+        this.credentials.email = event.target.value;
+    }
+
+    @action
+    updatePassword(event) {
+        this.credentials.password = event.target.value;
     }
 }
