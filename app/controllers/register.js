b44cb522cc57c90a05c0e0cb74ee2f04bb92c5db
Dean Longmire <deanlongmire100@gmail.com>
2023-06-01 21:50:13 -0400

    Create user on register request

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,21 +1,40 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class RegisterController extends Controller {
     @service router;
+    @tracked credentials = {
+        email: "",
+        password: ""
+    };
 
     @action
     loginClick() {
         fetch("http://localhost:5000/users/register", {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.credentials)
         })
         .then(response => {
             if(response.ok)
             {
-                console.log("Connected!");
+                console.log("Account Created!");
                 this.router.transitionTo('team-home');
             }   
         });
+    }
+
+    @action
+    updateEmail(event) {
+        this.credentials.email = event.target.value;
+    }
+
+    @action
+    updatePassword(event) {
+        this.credentials.password = event.target.value;
     }
 }
